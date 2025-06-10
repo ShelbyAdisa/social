0a9cf3da418d414a378e50f5f82b5ca4920556ba
ShelbyAdisa <sheladisaa@gmail.com>
2025-06-10 11:02:32 +0300

    social

--- social_media_project/settings.py
+++ social_media_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0%hdg%@mw=q_nhrp7en=u&e_$+4z5w8xu94fz&8c++%0p_2+65
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
